b3f20d1f18373c2c247fc434c74b7a279c0e1ff6
lorinma <malingreal@gmail.com>
2016-08-14 20:48:07 +0300

    auth0 setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  providers: [AuthService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title = 'SeeBIM';
+  constructor(private auth: AuthService) {
+
+  }
+  ngOnInit(){
+    if(!this.auth.authenticated()){
+      this.auth.login();
+    }
+  }
 }
