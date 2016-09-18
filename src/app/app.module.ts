9e9d89a05330dd41b177cc10c4b381325ab41f5e
Ling Ma <malingreal@gmail.com>
2016-09-18 09:47:53 +0000

    add project-list; fix hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,22 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { appRoutingProviders, routing } from "./app.routing";
+import { ProjectListComponent } from './dashboard/project-list/project-list.component';
+
+// import {DashboardModule} from "./dashboard/dashboard.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    ProjectListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     routing,
+    // DashboardModule,
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import {MdCardModule} from "@angular2-material/card";
+import {MdToolbarModule} from "@angular2-material/toolbar";
+import {MdButtonModule} from "@angular2-material/button";
+import {MdSidenavModule} from "@angular2-material/sidenav";
+import {MdListModule} from "@angular2-material/list";
+import {MdIconModule} from "@angular2-material/icon";
+import {MdTabsModule} from "@angular2-material/tabs";
+import {MdInputModule} from "@angular2-material/input";
+
+import {DashboardComponent} from "./dashboard.component";
+import {ProjectListComponent} from "./project-list/project-list.component";
+// import {ModelListComponent} from "./model-list/model-list.component";
+// import {ModelViewerComponent} from "./model-viewer/model-viewer.component";
+
+// import {dashboardRouting} from "./dashboard.routing";
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+    // , ModelListComponent, ModelViewerComponent, ProjectListComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    // dashboardRouting,
+    // MdCardModule,
+    // MdToolbarModule,
+    // MdButtonModule,
+    // MdSidenavModule,
+    // MdListModule,
+    // MdIconModule,
+    // MdTabsModule,
+    // MdInputModule,
+  ],
+  providers: [
+  ],
+})
+export class DashboardModule {
+
+}
