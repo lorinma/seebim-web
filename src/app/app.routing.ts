b3775f85f9f9bd7f69d324a7fa1fa07565d7e52d
Ling Ma <malingreal@gmail.com>
2016-09-19 18:32:30 +0000

    clean

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
 const appRoutes: Routes = [
   { path: '', component: AppComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '' },
 ];
 
